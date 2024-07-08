76aa72f6a25b80d8db1ce80bd875d66081570b62
=Punit2709 <=punitsp2003@gmail.com>
2024-07-08 15:50:49 +0530

    Day-5 Done

---
+++ Day-5/Day-5/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Day_5.Models
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class User
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
